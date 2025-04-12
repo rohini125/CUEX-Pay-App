3686c42c006b0b646c7211e33b4988188d5db2be
sakshi parkale <120358706+SakshiParkale@users.noreply.github.com>
2025-04-12 23:20:50 +0530

    add forgot password routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,7 +8,7 @@ import {
   deleteUserAccount,
   logoutUser,
   resetPassword,
-  requestPasswordReset,
+  forgotPasswordInfo,
 } from "../controllers/userController.js";
 // import { registerUser, loginUser, verifyOtp } from '../controllers/authController.js';
 
@@ -18,10 +18,17 @@ router.post("/signup", registerUser);
 router.post("/login", loginUser);
 // router.post("/request-send-otp",requestSendOtp);
 router.post("/verify-otp", verifyOtp);
-router.post("/resend-otp", resendOtp,);
+router.post("/resend-otp", resendOtp);
 router.post("/deleteAccount", deleteUserAccount);
 router.post("/logout", logoutUser);
-router.get("/reset-password", resetPassword);
-router.post("/request-password-reset", requestPasswordReset);
+
+// GET user info for forgot password
+router.post("/forgot-password", forgotPasswordInfo);
+
+// POST reset password
+router.post("/reset-password", resetPassword);
+
+// router.get("/reset-password", resetPassword);
+// router.post("/request-password-reset", requestPasswordReset);
 
 export default router;
